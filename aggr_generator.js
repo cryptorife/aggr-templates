707d544c23b65d19158db58990151263815fc10f
cryptorife <cryptorife@pm.me>
2023-04-14 16:11:56 +0300

    add full, chart and tape templates

--- aggr_generator.js
+++ aggr_generator.js
@@ -1,7 +1,14 @@
 const fs = require('fs')
 const {markets} = require('./markets.json')
-const template = require("./template.json")
-const values = require("./values.json");
+const template_chart = require("./template_chart.json")
+const template_full = require("./template_full.json")
+const template_tape = require("./template_tape.json")
+
+const templates = {
+  chart: template_chart,
+  full: template_full,
+  tape: template_tape
+}
 
 const baseQuoteLookupKnown = new RegExp(
     `^([A-Z0-9]{3,})[-/:]?(USDT|USDC|TUSD|BUSD)$|^([A-Z0-9]{2,})[-/:]?(UST|EUR|USD)$`
@@ -20,7 +27,8 @@ function getMarketProduct(exchangeId, symbol, noStable) {
     } else if (exchangeId === 'BINANCE_FUTURES' || exchangeId === 'DYDX') {
       type = 'perp'
     } else if (exchangeId === 'BITFINEX' && /F0$/.test(symbol)) {
-      type = 'perp'
+      // type = 'perp'
+      type = 'future' // changed to future to avoid it, as it is not parsing it well
     } else if (exchangeId === 'HUOBI' && /_(CW|CQ|NW|NQ)$/.test(symbol)) {
       type = 'future'
     } else if (exchangeId === 'HUOBI' && /-/.test(symbol)) {
@@ -130,17 +138,25 @@ function getMarketProduct(exchangeId, symbol, noStable) {
     }
   }
 
-function getTemplate(symbol, markets, cvdSpot, cvdPerp, deltaSpot, deltaPerp, basis) {
-  const title = `Rife${symbol} Lite`;
-  const id = `rife${symbol}lite_v` + Date.now()
+function getTemplate(template, type, symbol, markets, spotFlatMarkets, perpFlatMarkets, cvdSpot, cvdPerp, deltaSpot, deltaPerp, basis) {
+  const title = `Rife${symbol} ${type}`;
+  const id = `rife${symbol}${type}_v` + Date.now()
   template.name = title;
   template.id = id + Date.now()
-  template.states.panes.panes.chart.markets = markets;
-  template.states.chart.indicators["cvd-spot"].script = cvdSpot
-  template.states.chart.indicators["cvd-perp"].script = cvdPerp
-  template.states.chart.indicators["delta-spot"].script = deltaSpot
-  template.states.chart.indicators["delta-perp"].script = deltaPerp
-  template.states.chart.indicators["basis"].script = basis
+  if (type !== 'tape') {
+    template.states.panes.panes.chart.markets = markets;
+    template.states.chart.indicators["cvd-spot"].script = cvdSpot
+    template.states.chart.indicators["cvd-perp"].script = cvdPerp
+    template.states.chart.indicators["delta-spot"].script = deltaSpot
+    template.states.chart.indicators["delta-perp"].script = deltaPerp
+    template.states.chart.indicators["basis"].script = basis
+  }
+  if (type !== 'chart') {
+    template.states.panes.panes.stats.markets = perpFlatMarkets
+    template.states.panes.panes.stats2.markets = spotFlatMarkets
+    template.states.panes.panes.trades.markets = perpFlatMarkets
+    template.states.panes.panes.trades2.markets = spotFlatMarkets
+  }
   return template;
 }
 
@@ -164,29 +180,32 @@ getDelta = (market, type) => {
 
 const marketsBySymbol = {};
 for (const market of markets) {
-    [exchange, symbol] = market.split(':');
-    if (exchange === 'FTX') continue;
-    const m = getMarketProduct(exchange, symbol)
-    if (marketsBySymbol[m.base] === undefined) marketsBySymbol[m.base] = {spot: [], perp: []};
-    marketsBySymbol[m.base][m.type].push(m);
-  }
+  [exchange, symbol] = market.split(':');
+  if (exchange === 'FTX') continue;
+  const m = getMarketProduct(exchange, symbol)
+  if (m.type === 'future') continue;
+  if (m.quote === undefined || m.quote.indexOf('USD') === -1) continue;
+  if (marketsBySymbol[m.base] === undefined) marketsBySymbol[m.base] = {spot: [], perp: []};
+  marketsBySymbol[m.base][m.type].push(m);
+}
 
-  for (const symbol of Object.keys(marketsBySymbol).filter(m => !['USDT', 'BT', 'ET', 'SENTIM', 'SO'].includes(m))) {
-  const m = marketsBySymbol[symbol];
-  const cvdSpot = getCvd(m, 'spot');
-  const cvdPerp = getCvd(m, 'perp');
-  const deltaSpot = getDelta(m, 'spot');
-  const deltaPerp = getDelta(m, 'perp');
-  const basis = getBasis(m);
-  const flattenedMarkets = [
-    ...marketsBySymbol[symbol].spot.map(m => m.id),
-    ...marketsBySymbol[symbol].perp.map(m => m.id)
-  ];
-  const temp = getTemplate(symbol, flattenedMarkets, cvdSpot, cvdPerp, deltaSpot, deltaPerp, basis);
-  fs.writeFile(`./gen/Rife${symbol}-lite.json`, JSON.stringify(temp), 'utf8', err => {
-    if (err) console.log('error', err)
-    else console.log('done')
-  })
+for (const symbol of Object.keys(marketsBySymbol).filter(m => !['USDT'].includes(m))) {
+  const types = ["chart", "full", "tape", ];
+  for (const type of types) {
+    const m = marketsBySymbol[symbol];
+    const cvdSpot = getCvd(m, 'spot');
+    const cvdPerp = getCvd(m, 'perp');
+    const deltaSpot = getDelta(m, 'spot');
+    const deltaPerp = getDelta(m, 'perp');
+    const basis = getBasis(m);
+    const spotFlatMarkets = marketsBySymbol[symbol].spot.map(m => m.id);
+    const perpFlatMarkets = marketsBySymbol[symbol].perp.map(m => m.id);
+    const flattenedMarkets = [...spotFlatMarkets, ...perpFlatMarkets];
+    const temp = getTemplate(templates[type], type, symbol, flattenedMarkets, spotFlatMarkets, perpFlatMarkets, cvdSpot, cvdPerp, deltaSpot, deltaPerp, basis);
+    fs.writeFile(`./templates/${type}/Rife${symbol}-${type}.json`, JSON.stringify(temp), 'utf8', err => {
+      if (err) console.log('error', err)
+    })
+  }
 }
 
 
